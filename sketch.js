2bf46410d716f302e2ca893185cb214507e5d997
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2023-12-20 21:32:19 -0600

    Add video funtionality

--- sketch.js
+++ sketch.js
@@ -1,7 +1,11 @@
+let video;
+
 function setup() {
   createCanvas(400, 400, WEBGL);
+  video = createCapture(VIDEO);
 }
 
 function draw() {
   background(220);
+  image(video, 0, 0, width, (width * video.height) / video.width);
 }
