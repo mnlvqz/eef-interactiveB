fa66cd249eb65d1a4f5bfce91c892ef4bdc2958d
mnlvqz <44558123+mnlvqz@users.noreply.github.com>
2023-12-20 21:38:36 -0600

    Update video functionality

--- sketch.js
+++ sketch.js
@@ -1,11 +1,23 @@
-let video;
+let videoIn;
 
 function setup() {
   createCanvas(400, 400, WEBGL);
-  video = createCapture(VIDEO);
+  videoIn = createCapture(VIDEO, {
+    audio: false,
+    video: {
+      /*
+      width: 280,
+      height: 280,
+      */
+      aspectRatio: 1,
+      facingMode: {
+        exact: "environment",
+      },
+    },
+  });
 }
 
 function draw() {
   background(220);
-  image(video, 0, 0, width, (width * video.height) / video.width);
+  image(videoIn, 0, 0, width, (width * videoIn.height) / videoIn.width);
 }
